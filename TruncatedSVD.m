9c42d750930489e88be971a01b1d4456a3cf4c98
bryceshirley <114733577+bryceshirley@users.noreply.github.com>
2023-04-28 19:10:00 +0100

    Update TruncatedSVD.m
    Add descriptive comments

--- TruncatedSVD.m
+++ TruncatedSVD.m
@@ -1,12 +1,15 @@
 function [Ut,St,Vt]=TruncatedSVD(A,tau)
+% Compute the Classical SVD
 [U,S,V]=svd(A,'econ');
 
-% norm(A,'fro') == norm(S,'fro')
+% Recall that ||A||_fro = ||S||_fro where ||.||_fro is the Frobenius norm defined by the sqrt of the sum of squared elements.
 % Vector of singular values
 s=diag(S);
 
+% Truncate singular values that satisfy the following condition based on an Error Tolerence of tau
 r=sum(cumsum(flip(s).^2) > (tau*norm(s,'fro'))^2);
 
+% Truncate the SVD to from a rank-r approximation.
 Ut = U(:,1:r);
 St = diag(s(1:r));
 Vt = V(:,1:r);
